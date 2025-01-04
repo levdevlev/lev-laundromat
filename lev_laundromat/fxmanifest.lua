67ab70a389909c8193a1acc935ef2d82f73cfe9e
levdevlev <110393325+levdevlev@users.noreply.github.com>
2025-01-04 10:16:42 +0100

    Add files via upload

---
+++ lev_laundromat/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+this_is_a_map 'yes'
+lua54 'yes'
+
+files {
+    'laundromat_doors_game.dat151.rel',
+    'lev_laundromat.dat151.rel',
+}
+
+data_file 'AUDIO_GAMEDATA' 'laundromat_doors_game.dat'
+data_file 'AUDIO_GAMEDATA' 'lev_laundromat.dat'
